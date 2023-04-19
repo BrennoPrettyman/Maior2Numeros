c79f7d0ec54320c18587934252e65471d81ca197
Brenno Prettyman <brennoprettyman@gmail.com>
2023-04-19 11:43:59 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int Primeiro, Segundo, Maior;
+
+Console.Clear();
+
+Console.Write("Insira o Primeiro Número...: ");
+Primeiro = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Insira o Segundo Número....: ");
+Segundo = Convert.ToInt32(Console.ReadLine());
+
+if (Primeiro > Segundo)
+{
+    Maior = Primeiro;
+}
+
+else
+{
+    Maior = Segundo;
+}
+Console.WriteLine();
+
+Console.WriteLine($"O Maior número é...........: {Maior}");
